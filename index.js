46273f6f37b0083fbac440980f9bfc809dc63cb9
monsourcecode <dw_boubechtoula@esi.dz>
2020-05-13 10:38:00 +0100

    AUTRE COMMIT

--- index.js
+++ index.js
@@ -5,7 +5,3 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-let a = new Array() ; 
-a.push(1231);
-let n= a.filter(e=>e==131) ; 
-console.log(n); 
