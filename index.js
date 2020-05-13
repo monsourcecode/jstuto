25a4a3b530d78aec28597174cebc1bedc40ee4e0
monsourcecode <dw_boubechtoula@esi.dz>
2020-05-13 11:29:58 +0100

    baground  reset and base styling

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 // Import stylesheets
 import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
-let s = new String("walid") ; 
-let ss = s.toUpperCase(); 
-console.log(ss);
+ 
+ 
